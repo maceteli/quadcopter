0bb71c8061a356d3a2ac7e4ffb65551b9fbf5177
Golden Mac-Eteli <47808850+maceteli@users.noreply.github.com>
2021-05-29 16:23:16 +0100

    Add files via upload

--- quadcopter.py
+++ quadcopter.py
@@ -11,7 +11,7 @@ args = parser.parse_args()
 print ('Connecting to vehicle: %s' % args.connect)
 vehicle = connect('udp:127.0.0.1:14551')
     
-def arm_and_takeoff():
+def arm_and_takeoff(aTargetAltitude):
     """
     Arms vehicle and fly to aTargetAltitude.
     """
@@ -20,29 +20,50 @@ def arm_and_takeoff():
     # Don't try to arm until autopilot is ready
     while not vehicle.is_armable:
        print(" Waiting for vehicle to initialise...")
-       time.sleep(10)
+       time.sleep(1)
 
     print("Arming motors")
     # Copter should arm in GUIDED mode
     vehicle.mode = VehicleMode("GUIDED")
     vehicle.armed = True
         
-    time.sleep(10)
+    time.sleep(1)
     # Confirm vehicle armed before attempting to take off
-    if(vehicle.armed == true):
+    if(vehicle.armed == True):
         print("vehicle armed successful")
-        print("vehicle disarming")
-        vehicle.armed = False
+        print("Taking off!")
+        vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
         
     else:
         print("vehicle armed unsuccesful")
-    
+        
+def Land():
+##This function ensures that the vehicle has landed (before vechile.close is called)
+
+  print("Landing")
+  ##thread_distance.join()
+  time.sleep(1)
+  vehicle.mode = VehicleMode("LAND")
+  while vehicle.armed:
+    time.sleep(1)
+  vehicle.close()   
+
+arm_and_takeoff(3)
+
+vehicle.airspeed = 0.5
 
 
-arm_and_takeoff()
+print("Travelling to waypoint 1 ...")
+point1 = LocationGlobalRelative(7.606651, 5.306836)
+vehicle.simple_goto(point1)
 
-time.sleep(10)
+# sleep so we can see the change in map
+time.sleep(5)
+
+print("Returning to Launch")
+vehicle.mode = VehicleMode("RTL")
+
+time.sleep(5)
 # Close vehicle object before exiting script
-print("Close vehicle object")
-vehicle.close()
+Land()
 
